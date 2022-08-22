93edf3c0a689c38a6c2139af36e38d7661025962
Maxim Vasilyev <64894489+mdvasilyev@users.noreply.github.com>
2022-08-22 15:56:38 +0500

    Update Gauss_method.py

--- Gauss_method.py
+++ Gauss_method.py
@@ -11,21 +11,20 @@ def remove_last_column(array):
     return array
 
 def first_elimination(array, n, m):
-    new_array = array
-    for i in range(n):
-        if i == 0:
-            coefficient = 0
-        else:
-            coefficient = array[i, i - 1] / array[i - 1, i - 1]
-        for j in range(m + 1):
-            new_array[i, j] = array[i, j] - coefficient * array[i - 1, j]
-    return new_array
+    for i in range(n - 1):              # i stands for major line for calculating coefficients
+        for k in range(n - i):          # k stands for lines below
+            if k == 0:
+                coefficient = 0
+            else:
+                coefficient = array[k + i, i] / array[i, i]
+            for j in range(m + 1):      # j stands for elements in each line
+                array[k + i, j] = array[k + i, j] - coefficient * array[i, j]
+    return array
 
 def main():
     n, m = map(int, input().split())
     array = get_an_array(n, m)
     new_array = first_elimination(array, n, m)
-    print(array)
     print(new_array)
 
 if __name__ == "__main__":
