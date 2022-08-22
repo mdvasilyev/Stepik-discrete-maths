3ee0c78e662c6097e05889c375b4b732f1408709
Maxim Vasilyev <64894489+mdvasilyev@users.noreply.github.com>
2022-08-22 16:51:47 +0500

    Update Gauss_method.py

--- Gauss_method.py
+++ Gauss_method.py
@@ -21,11 +21,42 @@ def first_elimination(array, n, m):
                 array[k + i, j] = array[k + i, j] - coefficient * array[i, j]
     return array
 
+def decision(full_rank, rank, number_of_variables):
+    need_output = False
+    if full_rank != rank:
+        print('NO')
+    elif full_rank == rank and rank < number_of_variables:
+        print('INF')
+    elif full_rank == rank and rank == number_of_variables:
+        need_output = True
+        print('YES')
+    return need_output
+        
+def back_substitution(eliminated_full_array, n):
+    variables = np.zeros(n)
+    for i in range(n - 1, -1, -1):
+        if i == n - 1:
+            variables[i] = eliminated_full_array[i, n] / eliminated_full_array[i, n - 1]
+        else:
+            sum = 0
+            for j in range(n - 1, i, -1):
+                sum = sum + variables[j] * eliminated_full_array[i, j]
+            variables[i] = (eliminated_full_array[i, n] - sum) / eliminated_full_array[i, i]
+    return variables
+
 def main():
-    n, m = map(int, input().split())
+    n, m = map(int, input().split())    # n - number of equations, m - number of variables
     array = get_an_array(n, m)
-    new_array = first_elimination(array, n, m)
-    print(new_array)
+    eliminated_full_array = first_elimination(array, n, m)
+    eliminated_array = np.delete(eliminated_full_array, -1, axis=1)
+    full_rank = np.linalg.matrix_rank(eliminated_full_array)
+    rank = np.linalg.matrix_rank(eliminated_array)
+    print(eliminated_full_array)
+    need_output = decision(full_rank, rank, m)
+    if need_output == True:
+        variables = back_substitution(eliminated_full_array, n)
+        for i in range(n):
+            print(variables[i], end=' ')
 
 if __name__ == "__main__":
     main()
