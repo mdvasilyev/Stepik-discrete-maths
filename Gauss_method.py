59d4fdb8ee868373ee1ef9d7c2b787b7b133ccc7
Maxim Vasilyev <64894489+mdvasilyev@users.noreply.github.com>
2022-08-25 20:04:08 +0500

    Update Gauss_method.py

--- Gauss_method.py
+++ Gauss_method.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def get_an_array(n, m):
-    array = np.zeros((n, m + 1), dtype=int)
+    array = np.zeros((n, m + 1), dtype=float)
     for i in range(n):
         array[i] = [float(item) for item in input().split()]
     return array
@@ -11,19 +11,21 @@ def remove_last_column(array):
     return array
 
 def first_elimination(array, n, m):
-    for i in range(n - 1):              # i stands for major line for calculating coefficients
+    for i in range(m - 1):                 # i stands for major line for calculating coefficients
         if array[i, i] == 0:
             coef_array = array[:, [i]]
+            indexes = np.arange(i)
+            coef_array = np.delete(coef_array, indexes)
             good_index = np.transpose(np.nonzero(coef_array))[:, [0]][-1][0]
             array[[i, good_index]] = array[[good_index, i]]
         else:
-            continue
-        for k in range(n - i):          # k stands for lines below
+            pass
+        for k in range(n - i):             # k stands for lines below
             if k == 0:
                 coefficient = 0
             else:
                 coefficient = array[k + i, i] / array[i, i]
-            for j in range(m + 1):      # j stands for elements in each line
+            for j in range(i, m + 1):      # j stands for elements in each line
                 array[k + i, j] = array[k + i, j] - coefficient * array[i, j]
     return array
 
@@ -38,16 +40,16 @@ def decision(full_rank, rank, number_of_variables):
         print('YES')
     return need_output
         
-def back_substitution(eliminated_full_array, n):
-    variables = np.zeros(n)
-    for i in range(n - 1, -1, -1):
-        if i == n - 1:
-            variables[i] = eliminated_full_array[i, n] / eliminated_full_array[i, i]
+def back_substitution(eliminated_full_array, m):
+    variables = np.zeros(m)
+    for i in range(m - 1, -1, -1):
+        if i == m - 1:
+            variables[i] = eliminated_full_array[i, m] / eliminated_full_array[i, i]
         else:
             sum = 0
-            for j in range(n - 1, i, -1):
+            for j in range(m - 1, i, -1):
                 sum = sum + variables[j] * eliminated_full_array[i, j]
-            variables[i] = (eliminated_full_array[i, n] - sum) / eliminated_full_array[i, i]
+            variables[i] = (eliminated_full_array[i, m] - sum) / eliminated_full_array[i, i]
     return variables
 
 def main():
@@ -60,8 +62,8 @@ def main():
     rank = np.linalg.matrix_rank(eliminated_array)
     need_output = decision(full_rank, rank, m)
     if need_output == True:
-        variables = back_substitution(eliminated_full_array, n)
-        for i in range(n):
+        variables = back_substitution(eliminated_full_array, m)
+        for i in range(m):
             print(variables[i], end=' ')
 
 if __name__ == "__main__":
