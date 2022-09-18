32fa3c8d0cb0d22c408a590f1c168f65980b5de1
Maxim Vasilyev <64894489+mdvasilyev@users.noreply.github.com>
2022-09-18 18:50:46 +0300

    final accepted version

--- Combination.py
+++ Combination.py
@@ -1,41 +1,39 @@
 from math import factorial
 import numpy as np
-import itertools
 
 def number_of_combinations(k, n):
-    res = factorial(n) / (factorial(k) * factorial(n - k))
-    return res
+    return factorial(n) / (factorial(k) * factorial(n - k))
 
 def recurrence(k, n):
     sum = 0
     i = k - 1
     while i <= n - 1:
-        summand = number_of_combinations(k - 1, i)
-        sum = sum + summand
+        sum += number_of_combinations(k - 1, i)
         i += 1
     return sum
 
 def all_combinations(k, num_of_elements):
-    elements = np.arange(k)
-    print(elements)
-    checker = -1
-    for i in range(-1, 1, -1):
-        if elements[i] <= num_of_elements - k + i + 1:
-            elements[i] = elements[i] + 1
-            for j in range(i, k):
-                elements[i] = elements[i - 1] + 1
-        else:
+    elements = np.arange(k, dtype=int)
+    while True:
+        for i in range(k):
+            print(elements[i], end=' ')
+        print()
+        m = -1
+        for i in range(k - 1, -1, -1):
+            if elements[i] <= num_of_elements - k + i - 1:
+                m = i
+                elements[i] += 1
+                break
+        if m == -1:
             break
+        for j in range(m + 1, k):
+            elements[j] = elements[j - 1] + 1
     
     
 
 def main():
     k, n = map(int, input().split())
-    array = array_filler(k, recurrence(k, n))
-    for i in itertools.combinations(np.arange(3), 2):
-        print(i, end=' ')
-    print(array)
-    print(recurrence(k, n))
+    all_combinations(k, n)
 
 if __name__ == "__main__":
     main()
