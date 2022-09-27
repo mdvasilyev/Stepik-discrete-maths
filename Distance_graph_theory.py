bf4e8e585d15e412731e6ebacfd42f91855806ae
Maxim Vasilyev <64894489+mdvasilyev@users.noreply.github.com>
2022-09-27 19:28:30 +0300

    Update Distance_graph_theory.py

---
+++ Distance_graph_theory.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def main():
+    v, e = map(int, input().split())
+    edge_array = np.zeros((e, 2))
+    for i in range(e):
+        edge_array[i, 0], edge_array[i, 1] = map(int, input().split())
+
+if __name__ == "__main__":
+    main()
