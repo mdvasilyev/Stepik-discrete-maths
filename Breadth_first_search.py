9c8438880cd6455a06eaccec6fdb86e0869526f1
Maxim Vasilyev <64894489+mdvasilyev@users.noreply.github.com>
2022-10-01 21:24:24 +0300

    final accepted version

--- Breadth_first_search.py
+++ Breadth_first_search.py
@@ -1,5 +1,4 @@
 from collections import deque
-from turtle import distance
 
 def adjacency_list(nodes_number: int, edge_array: list) -> dict:
     nodes_array = [i for i in range(nodes_number)]
@@ -11,38 +10,26 @@ def adjacency_list(nodes_number: int, edge_array: list) -> dict:
         adjacency_array[v].append(u)
     return adjacency_array
 
-def BFS(nodes_number: int, adjacency_list: dict, start_node: int, goal_node: int, distance: list) -> bool:
-    queue = deque()
+def BFS(adjacency_list: dict, nodes_number: int, start_node: int) -> list:
+    distances = [0 for _ in range(nodes_number)]
     is_discovered = [False for _ in range(nodes_number)]
-    distance = [10e6 for _ in range(nodes_number)]
-    is_discovered[start_node] = True
-    distance[start_node] = 0
+    queue = deque()
     queue.append(start_node)
-    while not queue:
-        v = queue.popleft()
-        for u in adjacency_list[v]:
-            if not is_discovered[u]:
-                is_discovered[u] = True
-                distance[u] += 1
-                queue.append(u)
-                if adjacency_list[u] == goal_node:
-                    return True
-    return False
-
-def calculate_distance(nodes_number: int, adjacency_list: dict, start_node: int, goal_node: int, distance: list):
-    if not BFS(nodes_number, adjacency_list, start_node, goal_node, distance):
-        print("Given start node and goal node are not connected")
-    else:
-        for i in range(nodes_number):
-            print(distance[i], end=' ')
+    is_discovered[start_node] = True
+    while len(queue) != 0:
+        current_node = queue.popleft()
+        for i in adjacency_list[current_node]:
+            if not is_discovered[i]:
+                is_discovered[i] = True
+                distances[i] = distances[current_node] + 1
+                queue.append(i)
+    return distances
 
 def main():
     v, e = map(int, input().split())
     edge_array = list(tuple(map(int, input().split())) for _ in range(e))
-    distance = [0 for _ in range(v)]
-    print(adjacency_list(v, edge_array))
-    for i in range(2):
-        calculate_distance(v, adjacency_list(v, edge_array), 0, i, distance)
+    for _, x in enumerate(BFS(adjacency_list(v, edge_array), v, 0)):
+        print(x, end=' ')
 
 if __name__ == "__main__":
     main()
