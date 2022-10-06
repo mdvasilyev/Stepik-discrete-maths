472b2a840c43a2edcd16e97e79cd3b5f8ce86a75
Maxim Vasilyev <64894489+mdvasilyev@users.noreply.github.com>
2022-10-06 20:48:24 +0300

    Update Eulerian_cycle.py
    need to fix index = path.index(node_to_start_with)

--- Eulerian_cycle.py
+++ Eulerian_cycle.py
@@ -1,8 +1,5 @@
 from collections import deque
 
-def flatten(l):
-    return [item for sublist in l for item in sublist]
-
 def remove_overlap(l: list) -> list:
     if l[0] == l[-1]:
         l.pop(-1)
@@ -29,7 +26,6 @@ def pave_a_path(start_node: int, adj_list: dict, path: list) -> list:
     queue.append(start_node)
     cur_node = start_node
     while bool(queue):
-        pred = cur_node
         cur_node = queue.popleft()
         for i, x in enumerate(adj_list[cur_node]):
             path.append(adj_list[cur_node][i])
@@ -37,24 +33,20 @@ def pave_a_path(start_node: int, adj_list: dict, path: list) -> list:
             adj_list[cur_node].pop(i)
             adj_list[x].pop(adj_list[x].index(cur_node))
             break
-    if len(flatten(list(adj_list.values()))) != 0:
-        adj_list[cur_node].append(pred)
-        adj_list[pred].append(cur_node)
     return path, adj_list
 
-def check_path(path: list, nodes_number: int, adj_list: dict):
+def find_a_node_to_start_with(path: list, adj_list: dict, nodes_number: int) -> int:
     nodes_array = [i + 1 for i in range(nodes_number)]
     for i in nodes_array:
         for j in adj_list[i]:
             if i in path and j not in path:
-                start_node = j
-                return start_node
-    return False
+                node_to_start_with = i
+                return node_to_start_with
 
-def add_a_cycle(cur_node: int, path: list, adj_list: dict) -> list:
-    index = path.index(cur_node)
+def add_a_cycle(node_to_start_with: int, path: list, adj_list: dict) -> list:
+    index = path.index(node_to_start_with)
     queue = deque()
-    queue.append(cur_node)
+    queue.append(node_to_start_with)
     while bool(queue):
         index += 1
         cur_node = queue.popleft()
@@ -64,26 +56,22 @@ def add_a_cycle(cur_node: int, path: list, adj_list: dict) -> list:
             adj_list[cur_node].pop(i)
             adj_list[x].pop(adj_list[x].index(cur_node))
             break
-    return path
+    return path, adj_list
 
 def main():
     v, e = map(int, input().split())
     edge_array = list(tuple(map(int, input().split())) for _ in range(e))
     adj_list = adjacency_list(v, edge_array)
-    if not check_necessity(adj_list):
+    if v == 0 or not check_necessity(adj_list):
         print("NONE")
     else:
         start_node = 1
         path = [start_node]
         euler_cycle, adj_list = pave_a_path(1, adj_list, path)
+        while len(euler_cycle) != e + 1:
+            node_to_start_with = find_a_node_to_start_with(euler_cycle, adj_list, v)
+            euler_cycle, adj_list = add_a_cycle(node_to_start_with, euler_cycle, adj_list)
         euler_cycle = remove_overlap(euler_cycle)
-        while len(euler_cycle) != e:
-            euler_cycle = remove_overlap(euler_cycle)
-            if type(check_path(euler_cycle, v, adj_list)) == bool:
-                break
-            else:
-                start_node = check_path(euler_cycle, v, adj_list)
-                pave_a_path(start_node, adj_list, euler_cycle)
         for i in euler_cycle:
             print(i, end=' ')
 
