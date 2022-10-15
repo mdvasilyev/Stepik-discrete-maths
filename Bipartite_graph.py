06c7521fc8a7814540628c924cfebcb98d856e71
Maxim Vasilyev <64894489+mdvasilyev@users.noreply.github.com>
2022-10-15 17:14:31 +0300

    implemented modified DFS algorithm

--- Bipartite_graph.py
+++ Bipartite_graph.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 def adjacency_list(nodes_number: int, edge_array: list) -> dict:
     nodes_array = [i + 1 for i in range(nodes_number)]
     adjacency_array = {}
@@ -10,30 +8,16 @@ def adjacency_list(nodes_number: int, edge_array: list) -> dict:
         adjacency_array[v].append(u)
     return adjacency_array
 
-# def DFS(v_array: list, iterator: int, edge_array: list) -> None:
-#     v_array[iterator] = True
-#     neighbours_array = np.array([])
-#     for j in range(len(edge_array)):
-#         if edge_array[j, 0] == iterator + 1:
-#             neighbours_array = np.append(neighbours_array, edge_array[j, 1])
-#         elif edge_array[j, 1] == iterator + 1:
-#             neighbours_array = np.append(neighbours_array, edge_array[j, 0])
-#     for j in neighbours_array:
-#         j = int(j)
-#         if not v_array[j - 1]:
-#             DFS(v_array, j - 1, edge_array)
-
-def DFS(adjacency_list: dict, start_node: int, color_array: list, queue: deque, is_discovered: list) -> list:
-    queue.append(start_node)
-    counter = 1
-    cur_node = queue.popleft()
-    for i in adjacency_list[cur_node]:
+def mod_DFS(adjacency_list: dict, start_node: int, color_array: list, is_discovered: list) -> list:
+    is_discovered[start_node - 1] = True
+    pred = start_node
+    for i in adjacency_list[start_node]:
         if not is_discovered[i - 1]:
-            is_discovered[i - 1] = True
-            color_array[i - 1] = counter % 2
-            queue.append(i)
-            break
-    counter += 1
+            color_array[i - 1] = color_array[pred - 1] + 1
+            color_array[i - 1] %= 2
+            adjacency_list[pred].pop(adjacency_list[pred].index(i))
+            adjacency_list[i].pop(adjacency_list[i].index(pred))
+            mod_DFS(adjacency_list, i, color_array, is_discovered)  
     return color_array
 
 def main():
@@ -41,8 +25,22 @@ def main():
     edge_array = list(tuple(map(int, input().split())) for _ in range(e))
     color_array = [0] * v
     adj_list = adjacency_list(v, edge_array)
-    queue = deque()
     is_discovered = [False] * v
+    start_node = 1
+    mod_DFS(adj_list, start_node, color_array, is_discovered)
+    while False in is_discovered:
+        next_node = -1
+        for i in adj_list:
+            if is_discovered[i - 1] == True and len(adj_list[i]) != 0:
+                next_node = i
+                break
+        else:
+            for i in adj_list:
+                if len(adj_list[i]) != 0:
+                    next_node = i
+                    break
+        mod_DFS(adj_list, next_node, color_array, is_discovered)
+    print(color_array)
 
 if __name__ == "__main__":
     main()
